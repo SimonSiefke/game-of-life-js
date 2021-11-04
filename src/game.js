c38e0228470d61a66a4fda581c75a81889000f78
Simon Siefke <simon.siefke@gmail.com>
2021-11-04 15:39:22 +0100

    add count neightbors function

---
+++ src/game.js
@@ -0,0 +1,74 @@
+const getTopLeft = (state, i, j) => {
+  if (i > 0 && j > 0) {
+    return state[i - 1][j - 1];
+  }
+  return 0;
+};
+
+const getTop = (state, i, j) => {
+  if (i > 0) {
+    return state[i - 1][j];
+  }
+  return 0;
+};
+
+const getTopRight = (state, i, j) => {
+  if (i > 0 && j < state[i].length - 1) {
+    return state[i - 1][j + 1];
+  }
+  return 0;
+};
+
+const getLeft = (state, i, j) => {
+  if (j > 0) {
+    return state[i][j - 1];
+  }
+  return 0;
+};
+
+const getRight = (state, i, j) => {
+  if (j < state[i].length - 1) {
+    return state[i][j + 1];
+  }
+  return 0;
+};
+
+const getBottomLeft = (state, i, j) => {
+  if (i < state.length - 1 && j > 0) {
+    return state[i + 1][j - 1];
+  }
+  return 0;
+};
+
+const getBottom = (state, i, j) => {
+  if (i < state.length - 1) {
+    return state[i + 1][j];
+  }
+  return 0;
+};
+
+const getBottomRight = (state, i, j) => {
+  if (i < state.length - 1 && j < state[i].length - 1) {
+    return state[i + 1][j + 1];
+  }
+  return 0;
+};
+
+export const countNeighbors = (state, i, j) => {
+  const topLeft = getTopLeft(state, i, j);
+  const top = getTop(state, i, j);
+  const topRight = getTopRight(state, i, j);
+  const left = getLeft(state, i, j);
+  const right = getRight(state, i, j);
+  const bottomLeft = getBottomLeft(state, i, j);
+  const bottom = getBottom(state, i, j);
+  const bottomRight = getBottomRight(state, i, j);
+  return (
+    topLeft + top + topRight + left + right + bottomLeft + bottom + bottomRight
+  );
+};
+
+export const getNextState = (state) => {
+  // TODO
+  return state;
+};
--- test/game.test.js
+++ test/game.test.js
@@ -1,3 +1,61 @@
+import { getNextState, countNeighbors } from "../src/game.js";
+
 test("game", () => {
   expect(1).toBe(1);
+});
+
+test("countNeighbors - underpopulation", () => {
+  expect(
+    countNeighbors(
+      [
+        [0, 0, 0],
+        [0, 1, 0],
+        [0, 0, 1],
+      ],
+      1,
+      1
+    )
+  ).toBe(1);
+});
+
+test("countNeighbors - survival", () => {
+  expect(
+    countNeighbors(
+      [
+        [0, 0, 0],
+        [1, 1, 0],
+        [0, 1, 1],
+      ],
+      1,
+      1
+    )
+  ).toBe(3);
+});
+
+test("countNeighbors - overcrowding", () => {
+  expect(
+    countNeighbors(
+      [
+        [1, 1, 1],
+        [1, 1, 1],
+        [1, 1, 1],
+      ],
+      1,
+      1
+    )
+  ).toBe(8);
+});
+
+test("countNeighbors - reproduction", () => {
+  expect(
+    countNeighbors(
+      [
+        [0, 0, 0],
+        [1, 0, 0],
+        [0, 1, 1],
+      ],
+      1,
+      1
+    )
+  ).toBe(3);
 });
